a1359b2f65829b4ad757a9ae660654934c59ba66
LL <NZLILI2015@gmail.com>
2017-02-23 22:50:59 +1300

    Add project files.

---
+++ BasicAuthDemo/Controllers/DemoDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BasicAuthDemo.Controllers
+{
+    [Authorize]
+    public class DemoDataController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { demoData = "You have to be authenticated to access this!" });
+        }
+    }
+}
